7fae51b5144a073ab5cbee1864c8e0eabebecbd8
Antony <antony.medinagarcia24@gmail.com>
2024-07-13 23:05:28 -0600

    add.: nuevo logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar=()=> {
   return (
     
       <nav className="flex w-full items-center justify-between p-[20px] lg:px-8 top-0 bg-darkGray" aria-label="Global" >
-        <img src="/public/img/logo.png" alt="logo" className="h-10 w-20" />
+        <img src="/public/img/LogoMain.png" alt="logo" className="h-10 w-20" />
         <div className="flex flex-row w-full justify-end">
           <a to="/" className="-mx-0.2 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-200 hover:bg-gray-600 ">Home</a>
           <button className="-mx-0.2 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-200 hover:bg-gray-600 " onClick={()=>displayForm(stateModal,setOpenModal)}>New Video</button>
